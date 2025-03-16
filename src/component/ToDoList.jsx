e65022d299e7373d484636eec3c8abd177753884
MuhammadElham <elhamwaheed777@gmail.com>
2025-03-16 15:09:14 -0700

    Assignment Done

--- src/component/ToDoList.jsx
+++ src/component/ToDoList.jsx
@@ -7,14 +7,34 @@ const ToDoList = () => {
     setNewTasks(e.target.value);
   };
   const addTasks = () => {
-    let result = setTasks((tasks) => [...tasks, newtasks]);
-    console.log(result);
+    setTasks((tasks) => [...tasks, newtasks]);
+    setNewTasks("");
+  };
+
+  const deleteItem = () => {
+
   };
   return (
-    <div className="flex items-center justify-center w-full h-screen">
-      <input type="text" placeholder="Enter Your Task" value={newtasks} onChange={handleInputChange} />
-      <button onClick={addTasks}>Add Item</button>
-    </div>
+    <>
+      <div className="flex flex-col justify-center items-stretch w-full h-screen px-10">
+        <div className="flex gap-4">
+          <input type="text" placeholder="Enter Your Task" value={newtasks} onChange={handleInputChange} />
+          <button onClick={addTasks}>Add Item</button>
+        </div>
+
+        <ul className="mx">
+          {tasks.map((item, index) => (
+            <li key={index} className="flex items-center justify-between">
+              <p>{item}</p>
+              <div>
+                <span onClick={editItem}>Edit</span>
+                <span onClick={deleteItem}>Delete</span>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 };
 
